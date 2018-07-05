0fc9065ddb21cb1cd296dafab9abd1c41942e87f
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-05 17:40:13 +0300

    Added 1st exercise problem.

---
+++ 01. Stack-And-Queues/Exercises 01. Reverse Numbers with a Stack/ReverseNumberswithaStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises_01._Reverse_Numbers_with_a_Stack
+{
+    class ReverseNumberswithaStack
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            var stack = new Stack<int>();
+
+            foreach (var number in input)
+            {
+                stack.Push(int.Parse(number));
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
