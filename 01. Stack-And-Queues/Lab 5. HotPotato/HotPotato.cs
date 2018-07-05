b2c1ab2299d227d756d99c1c58f109b36c046442
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-05 16:04:18 +0300

    Added 6th lab exercise.

--- 01. Stack-And-Queues/Lab 5. HotPotato/HotPotato.cs
+++ 01. Stack-And-Queues/Lab 5. HotPotato/HotPotato.cs
@@ -13,7 +13,7 @@
 
             while (queue.Count != 1)
             {
-                for (int i = 0; i < number; i++)
+                for (int i = 1; i < number; i++)
                 {
                     queue.Enqueue(queue.Dequeue());
                 }
