beb22ae3acf0d943043fb0415da6aa22834ea9df
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-05 15:46:33 +0300

    Added the 4th lab exercise.

---
+++ 01. Stack-And-Queues/Lab 4. MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab_4._MatchingBrackets
+{
+    using System;
+    using System.Collections.Generic;
+
+    class MatchingBrackets
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+                if (ch == '(')
+                    stack.Push(i);
+                else if (ch == ')')
+                {
+                    int startIndex = stack.Pop();
+                    string contents = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(contents);
+                }
+            }
+        }
+    }
+}
