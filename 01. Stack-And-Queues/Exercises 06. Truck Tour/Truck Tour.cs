133746236113b33cbf0a23c1b9e295680faaa76d
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-07 17:29:09 +0300

    Added 6th exercise problem.

---
+++ 01. Stack-And-Queues/Exercises 06. Truck Tour/Truck Tour.cs
@@ -0,0 +1,41 @@
+﻿namespace Exercises_06._Truck_Tour
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Truck_Tour
+    {
+        static void Main()
+        {
+            int totalPetrolPumps = int.Parse(Console.ReadLine());
+            int totalPetrol = 0;
+            var queue = new Queue<int[]>();
+            int startIndex = 0;
+
+            for (int i = 0; i < totalPetrolPumps; i++)
+            {
+                int[] petrolPump = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int amountPetrol = petrolPump[0];
+                int distance = petrolPump[1];
+                totalPetrol += amountPetrol;
+
+                if (totalPetrol >= distance)
+                {
+                    totalPetrol -= distance;
+                }
+                else
+                {
+                    totalPetrol = 0;
+                    startIndex = i + 1;
+                }
+            }
+
+            Console.WriteLine(startIndex);
+        }
+    }
+}
