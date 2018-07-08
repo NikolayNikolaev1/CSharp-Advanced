da772b1af766c5ef509853b360e089e9dac39e23
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-08 17:02:24 +0300

    Added 8th exercise problem.

---
+++ 01. Stack-And-Queues/Exercises 08. Recursive Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercises_08._Recursive_Fibonacci
+{
+    using System;
+
+    class Program
+    {
+        private static long[] fibNumbers;
+
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            fibNumbers = new long[number];
+            long result = GetFibonaci(number);
+            Console.WriteLine(result);
+        }
+
+        private static long GetFibonaci(int number)
+        {
+            if (number <= 2)
+            {
+                return 1;
+            }
+            if (fibNumbers[number - 1] != 0)
+            {
+                return fibNumbers[number - 1];
+            }
+            return fibNumbers[number - 1] =
+                GetFibonaci(number - 1) + GetFibonaci(number - 2);
+        }
+    }
+}
