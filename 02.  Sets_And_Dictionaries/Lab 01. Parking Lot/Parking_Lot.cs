38ba770cbcfff01a55f4f28d1571a1b4b64d38b7
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-09 18:05:34 +0300

    Added 1st lab problem

---
+++ 02.  Sets_And_Dictionaries/Lab 01. Parking Lot/Parking_Lot.cs
@@ -0,0 +1,44 @@
+﻿namespace Lab_01._Parking_Lot
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Parking_Lot
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            var parking = new HashSet<string>();
+
+            while (input != "END")
+            {
+                string[] inputParams = input.Split(", ");
+                string command = inputParams[0];
+                string carNumber = inputParams[1];
+
+                if (command == "IN")
+                {
+                    parking.Add(carNumber);
+                }
+                else if(command == "OUT")
+                {
+                    parking.Remove(carNumber);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
