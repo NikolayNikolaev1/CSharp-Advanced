1505a35d49eacb3a485a19ac4fe91592b1bd0f6e
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-09 18:22:10 +0300

    Added 4rd lab problem.

---
+++ 02.  Sets_And_Dictionaries/Lab 02. SoftUni Party/SoftUniParty.cs
@@ -0,0 +1,31 @@
+﻿namespace Lab_02._SoftUni_Party
+{
+    using System;
+    using System.Collections.Generic;
+
+    class SoftUniParty
+    {
+        static void Main()
+        {
+            var vip = new HashSet<string>();
+            var regular = new SortedSet<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "PARTY")
+                {
+                    break;
+                }
+                if ()
+                {
+                    vip.Add(input);
+                }
+                else
+                {
+                    regular.Add(input);
+                }
+            }
+        }
+    }
+}
---
+++ 02.  Sets_And_Dictionaries/Lab 03. Count Same Values in Array/Count_Same_Values_in_Array.cs
@@ -0,0 +1,36 @@
+﻿namespace Lab_03._Count_Same_Values_in_Array
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Count_Same_Values_in_Array
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            double[] numbers = input
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            var dictionary = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!dictionary.ContainsKey(number))
+                {
+                    dictionary.Add(number, 1);
+                }
+                else
+                {
+                    dictionary[number]++;
+                }
+            }
+
+            foreach (var num in dictionary)
+            {
+                Console.WriteLine($"{num.Key} - {num.Value} times");
+            }
+        }
+    }
+}
