d3d16dbf9c0932e467b6f9da2494d9b3d499b47b
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-10 16:48:53 +0300

    Added 4rd problem from exercises.

---
+++ 02.  Sets_And_Dictionaries/Problem 03. Periodic Table/Periodic_Table.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem_03._Periodic_Table
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Periodic_Table
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            var set = new SortedSet<string>();
+
+            for (int i = 0; i < input; i++)
+            {
+                string[] elements = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var el in elements)
+                {
+                    set.Add(el);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", set));
+        }
+    }
+}
