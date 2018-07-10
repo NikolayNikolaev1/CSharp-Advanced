83a9ec448a0907c392677d76b878ec27f33a3d3b
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-10 15:57:16 +0300

    Added 1st problem from exercises.

---
+++ 02.  Sets_And_Dictionaries/Problem 01. Unique Usernames/Unique_Usernames.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem_01._Unique_Usernames
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Unique_Usernames
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+            }
+
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
