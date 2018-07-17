3b73fa5c385e671384c2074602bff46efa91c1e3
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-17 16:23:59 +0300

    Added 1st exercise problem.

---
+++ 02.  Sets_And_Dictionaries/Exercises 01. Count Symbols/Count_Symbols.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercises_01._Count_Symbols
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Count_Symbols
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            var set = new SortedSet<char>();
+            var dic = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (dic.ContainsKey(input[i]))
+                {
+                    dic[input[i]]++;
+                }
+                else
+                {
+                    dic.Add(input[i], 1);
+                }
+            }
+
+            foreach (var d in dic)
+            {
+                Console.WriteLine($"{d.Key}: {d.Value} time/s");
+            }
+        }
+    }
+}
