a7ac68dd2ca6b51d90b7e3c12d11c989ac2198e7
HP-ELITEBOOK\Developer <n.nikolaevbg97@gmail.com>
2021-06-20 18:41:54 +0300

    started working on asp net exercises

---
+++ 02.  Sets_And_Dictionaries/The-V-Logger/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace The_V_Logger
+{
+    class Program
+    {
+        private const string STATISTICS_COMMAND = "statistics";
+        private const string SAVE_VLOGGER_COMMAND = "joined";
+        private const string FOLLOW_VLOGGER_COMMAND = "followed";
+        static void Main(string[] args)
+        {
+            // TODO: work in progres.....
+            HashSet<Vlogger> vloggerSet = new HashSet<Vlogger>();
+            bool hasNextCommand = true;
+
+            while (hasNextCommand)
+            {
+                string[] command = Console.ReadLine().Split();
+                string commandName = command[0];
+
+                if (!commandName.ToLower().Equals(STATISTICS_COMMAND))
+                {
+                    commandName = command[1];
+                }
+
+                switch (commandName.ToLower())
+                {
+                    case SAVE_VLOGGER_COMMAND:
+                        string vloggerName = command[0];
+                        Vlogger vlogger = new Vlogger(vloggerName);
+                        vloggerSet.Add(vlogger);
+                        break;
+                    case FOLLOW_VLOGGER_COMMAND:
+                        string followerName = command[0];
+                        string existingVloggerName = command[2];
+
+                        bool doesFollowerExists = vloggerSet.Any(v => v.Name.Equals(followerName));
+                        bool doesVloggerExists = vloggerSet.Any(v => v.Name.Equals(existingVloggerName));
+
+                        if (doesFollowerExists && doesVloggerExists)
+                        {
+                            Vlogger follower = vloggerSet
+                                .Where(v => v.Name.Equals(followerName))
+                                .FirstOrDefault();
+
+                            Vlogger following = vloggerSet
+                                .Where(v => v.Name.Equals(existingVloggerName))
+                                .FirstOrDefault();
+
+                            following.addFollower(follower);
+                            follower.addFollowing(following);
+                        }
+
+                        break;
+                    case STATISTICS_COMMAND:
+                        var vloggersByFollowers = vloggerSet.OrderByDescending(v => v.Followers.Count);
+
+                        Console.WriteLine($"The V-Logger has a total of {vloggerSet.Count} vloggers in its logs.");
+                        int counter = 1;
+                        foreach (var v in vloggersByFollowers)
+                        {
+                            Console.WriteLine($"{counter}. {v.Name} : {v.Followers.Count} followers, {v.Following.Count} following");
+                            if (v == vloggersByFollowers.First())
+                            {
+                                foreach (var vloggerFollower in v.Followers)
+                                {
+                                    Console.WriteLine($"* {vloggerFollower.Name}");
+                                }
+                            }
+                            counter++;
+                        }
+
+                        hasNextCommand = false;
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ 02.  Sets_And_Dictionaries/The-V-Logger/Vlogger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace The_V_Logger
+{
+    class Vlogger
+    {
+        private string name;
+        private HashSet<Vlogger> followers;
+        private HashSet<Vlogger> following;
+
+        public Vlogger(string name)
+        {
+            this.Name = name;
+            this.followers = new HashSet<Vlogger>();
+            this.following = new HashSet<Vlogger>();
+        }
+
+        public string Name { get => this.name; set => this.name = value; }
+
+        public IReadOnlyCollection<Vlogger> Followers { get => this.followers; }
+
+        public IReadOnlyCollection<Vlogger> Following { get => this.following; }
+
+        public void addFollower(Vlogger vlogger) 
+        {
+            if (!vlogger.Equals(this))
+            {
+                this.followers.Add(vlogger);
+            }
+        }
+        public void addFollowing(Vlogger vlogger) 
+        {
+            if (!vlogger.Equals(this)) {
+                this.following.Add(vlogger);
+            }
+        } 
+    }
+}
